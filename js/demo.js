bebb2438d1f524f40f83b052c01eadcba44bf033
Sebastian Tschan <sebastian.tschan@gmail.com>
2016-02-21 17:34:42 +0100

    Use JavaScript Standard Style.

--- js/demo.js
+++ js/demo.js
@@ -9,68 +9,66 @@
  * http://www.opensource.org/licenses/MIT
  */
 
-/*jslint unparam: true, regexp: true */
 /*global $ */
 
 $(function () {
-    'use strict';
+  'use strict'
 
-    // Load demo images from flickr:
-    $.ajax({
-        // Flickr API is SSL only:
-        // https://code.flickr.net/2014/04/30/flickr-api-going-ssl-only-on-june-27th-2014/
-        url: 'https://api.flickr.com/services/rest/',
-        data: {
-            format: 'json',
-            method: 'flickr.interestingness.getList',
-            api_key: '7617adae70159d09ba78cfec73c13be3' // jshint ignore:line
-        },
-        dataType: 'jsonp',
-        jsonp: 'jsoncallback'
-    }).done(function (result) {
-        var linksContainer = $('#links'),
-            baseUrl;
-        // Add the demo images as links with thumbnails to the page:
-        $.each(result.photos.photo, function (index, photo) {
-            baseUrl = 'https://farm' + photo.farm + '.static.flickr.com/' +
-                photo.server + '/' + photo.id + '_' + photo.secret;
-            $('<a/>')
-                .append($('<img>').prop('src', baseUrl + '_s.jpg'))
-                .prop('href', baseUrl + '_b.jpg')
-                .prop('title', photo.title)
-                .attr('data-dialog', '')
-                .appendTo(linksContainer);
-        });
-    });
+  // Load demo images from flickr:
+  $.ajax({
+    // Flickr API is SSL only:
+    // https://code.flickr.net/2014/04/30/flickr-api-going-ssl-only-on-june-27th-2014/
+    url: 'https://api.flickr.com/services/rest/',
+    data: {
+      format: 'json',
+      method: 'flickr.interestingness.getList',
+      api_key: '7617adae70159d09ba78cfec73c13be3' // jshint ignore:line
+    },
+    dataType: 'jsonp',
+    jsonp: 'jsoncallback'
+  }).done(function (result) {
+    var linksContainer = $('#links')
+    var baseUrl
+    // Add the demo images as links with thumbnails to the page:
+    $.each(result.photos.photo, function (index, photo) {
+      baseUrl = 'https://farm' + photo.farm + '.static.flickr.com/' +
+      photo.server + '/' + photo.id + '_' + photo.secret
+      $('<a/>')
+        .append($('<img>').prop('src', baseUrl + '_s.jpg'))
+        .prop('href', baseUrl + '_b.jpg')
+        .prop('title', photo.title)
+        .attr('data-dialog', '')
+        .appendTo(linksContainer)
+    })
+  })
 
-    // Initialize the theme switcher:
-    $('#theme-switcher').change(function () {
-        var theme = $('#theme');
-        theme.prop(
-            'href',
-            theme.prop('href').replace(
-                /[\w\-]+\/jquery-ui.css/,
-                $(this).val() + '/jquery-ui.css'
-            )
-        );
-    });
+  // Initialize the theme switcher:
+  $('#theme-switcher').change(function () {
+    var theme = $('#theme')
+    theme.prop(
+      'href',
+      theme.prop('href').replace(
+        /[\w\-]+\/jquery-ui.css/,
+        $(this).val() + '/jquery-ui.css'
+      )
+    )
+  })
 
-    // Initialize the effect switcher:
-    $('#effect-switcher').change(function () {
-        var value = $(this).val();
-        $('#blueimp-gallery-dialog').data({
-            show: value,
-            hide: value
-        });
-    });
+  // Initialize the effect switcher:
+  $('#effect-switcher').change(function () {
+    var value = $(this).val()
+    $('#blueimp-gallery-dialog').data({
+      show: value,
+      hide: value
+    })
+  })
 
-    // Initialize the slideshow button:
-    $('#slideshow-button')
-        .button({icons: {primary: 'ui-icon-image'}})
-        .on('click', function () {
-            $('#blueimp-gallery-dialog .blueimp-gallery')
-                .data('startSlideshow', true);
-            $('#links').children().first().click();
-        });
-
-});
+  // Initialize the slideshow button:
+  $('#slideshow-button')
+    .button({icons: {primary: 'ui-icon-image'}})
+    .on('click', function () {
+      $('#blueimp-gallery-dialog .blueimp-gallery')
+        .data('startSlideshow', true)
+      $('#links').children().first().click()
+    })
+})
--- js/jquery.image-gallery.js
+++ js/jquery.image-gallery.js
@@ -9,117 +9,112 @@
  * http://www.opensource.org/licenses/MIT
  */
 
-/*jslint unparam: true */
 /*global define, window, document */
 
-(function (factory) {
-    'use strict';
-    if (typeof define === 'function' && define.amd) {
-        define([
-            'jquery',
-            './blueimp-gallery'
-        ], factory);
-    } else {
-        factory(
-            window.jQuery,
-            window.blueimp.Gallery
-        );
-    }
+;(function (factory) {
+  'use strict'
+  if (typeof define === 'function' && define.amd) {
+    define([
+      'jquery',
+      './blueimp-gallery'
+    ], factory)
+  } else {
+    factory(
+      window.jQuery,
+      window.blueimp.Gallery
+    )
+  }
 }(function ($, Gallery) {
-    'use strict';
+  'use strict'
 
-    // Global click handler to open links with data-dialog attribute
-    // in the Gallery lightbox:
-    $(document).on('click', '[data-dialog]', function (event) {
-        // Get the widget id from the data-dialog attribute:
-        var id = $(this).data('dialog'),
-            widget = $(id),
-            dialogWidget = (widget.length && widget) ||
-                $('#blueimp-gallery-dialog'),
-            galleryWidget = dialogWidget.find('.blueimp-gallery'),
-            dialogInitialized = false,
-            // Initialize the dialog with custom options
-            // from data-attributes on the dialog widget:
-            dialogOptions = $.extend({
-                modal: true,
-                width: 'auto'
-            }, dialogWidget.data()),
-            galleryOptions = $.extend(
-                {
-                    toggleControlsOnReturn: true,
-                    toggleSlideshowOnSpace: true,
-                    enableKeyboardNavigation: true,
-                    startSlideshow: false
-                },
-                // Initialize the Gallery with custom options
-                // from data-attributes on the Gallery widget:
-                galleryWidget.data(),
-                {
-                    container: galleryWidget[0],
-                    index: this,
-                    event: event,
-                    carousel: true,
-                    onslide: function (index, slide) {
-                        var gallery = this,
-                            title = slide.firstChild.title;
-                        if (dialogInitialized) {
-                            dialogWidget.dialog('option', 'title', title);
-                        } else {
-                            dialogOptions.title = title;
-                            dialogWidget
-                                .data('gallery', this)
-                                .one('dialogclose', function () {
-                                    $(this).dialog('destroy');
-                                    gallery.handleClose();
-                                })
-                                .one('dialogresize', function () {
-                                    galleryWidget.css('width', 'auto');
-                                })
-                                .dialog(dialogOptions)
-                                .css({
-                                    visibility: 'visible',
-                                    height: 'auto',
-                                    overflow: 'visible'
-                                });
-                            dialogInitialized = true;
-                        }
-                        dialogWidget.trigger('slide', arguments);
-                    },
-                    onslideend: function () {
-                        dialogWidget.trigger('slideend', arguments);
-                    },
-                    onslidecomplete: function () {
-                        dialogWidget.trigger('slidecomplete', arguments);
-                    }
-                }
-            ),
-            // Select all links with the same data-dialog attribute:
-            links = $('[data-dialog="' + id + '"]'),
-            setGalleryWidgetDimensions = function () {
-                var width = $(window).width() -
-                        (dialogOptions.offsetWidth || 100),
-                    height = $(window).height() -
-                        (dialogOptions.offsetHeight || 150),
-                    // blueimp gallery carousel ratio (16/9)
-                    // to viewport ratio:
-                    ratio = ((16 / 9) / (width / height));
-                if (ratio < 1) {
-                    width = width * ratio;
-                }
-                galleryWidget.css('width', width);
-            };
-        if (galleryOptions.filter) {
-            links = links.filter(galleryOptions.filter);
+  // Global click handler to open links with data-dialog attribute
+  // in the Gallery lightbox:
+  $(document).on('click', '[data-dialog]', function (event) {
+    // Get the widget id from the data-dialog attribute:
+    var id = $(this).data('dialog')
+    var widget = $(id)
+    var dialogWidget = (widget.length && widget) ||
+                        $('#blueimp-gallery-dialog')
+    var galleryWidget = dialogWidget.find('.blueimp-gallery')
+    var dialogInitialized = false
+    // Initialize the dialog with custom options
+    // from data-attributes on the dialog widget:
+    var dialogOptions = $.extend({
+      modal: true,
+      width: 'auto'
+    }, dialogWidget.data())
+    var galleryOptions = $.extend(
+      {
+        toggleControlsOnReturn: true,
+        toggleSlideshowOnSpace: true,
+        enableKeyboardNavigation: true,
+        startSlideshow: false
+      },
+      // Initialize the Gallery with custom options
+      // from data-attributes on the Gallery widget:
+      galleryWidget.data(),
+      {
+        container: galleryWidget[0],
+        index: this,
+        event: event,
+        carousel: true,
+        onslide: function (index, slide) {
+          var gallery = this
+          var title = slide.firstChild.title
+          if (dialogInitialized) {
+            dialogWidget.dialog('option', 'title', title)
+          } else {
+            dialogOptions.title = title
+            dialogWidget
+              .data('gallery', this)
+              .one('dialogclose', function () {
+                $(this).dialog('destroy')
+                gallery.handleClose()
+              })
+              .one('dialogresize', function () {
+                galleryWidget.css('width', 'auto')
+              })
+              .dialog(dialogOptions)
+              .css({
+                visibility: 'visible',
+                height: 'auto',
+                overflow: 'visible'
+              })
+            dialogInitialized = true
+          }
+          dialogWidget.trigger('slide', arguments)
+        },
+        onslideend: function () {
+          dialogWidget.trigger('slideend', arguments)
+        },
+        onslidecomplete: function () {
+          dialogWidget.trigger('slidecomplete', arguments)
         }
-        // The Gallery only renders if the parent element is displayed,
-        // so we cannot use "display:none", but the following workaround:
-        dialogWidget.css({
-            visibility: 'hidden',
-            height: 0,
-            overflow: 'hidden'
-        });
-        setGalleryWidgetDimensions();
-        return new Gallery(links, galleryOptions);
-    });
-
-}));
+      }
+    )
+    // Select all links with the same data-dialog attribute:
+    var links = $('[data-dialog="' + id + '"]')
+    function setGalleryWidgetDimensions () {
+      var width = $(window).width() - (dialogOptions.offsetWidth || 100)
+      var height = $(window).height() - (dialogOptions.offsetHeight || 150)
+      // blueimp gallery carousel ratio (16/9) to viewport ratio:
+      var ratio = ((16 / 9) / (width / height))
+      if (ratio < 1) {
+        width = width * ratio
+      }
+      galleryWidget.css('width', width)
+    }
+    if (galleryOptions.filter) {
+      links = links.filter(galleryOptions.filter)
+    }
+    // The Gallery only renders if the parent element is displayed,
+    // so we cannot use "display:none", but the following workaround:
+    dialogWidget.css({
+      visibility: 'hidden',
+      height: 0,
+      overflow: 'hidden'
+    })
+    setGalleryWidgetDimensions()
+    return new Gallery(links, galleryOptions)
+  })
+}))
